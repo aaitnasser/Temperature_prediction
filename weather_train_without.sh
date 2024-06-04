9d386d4633ee02fc4712feed404981a636ca3425
Adnane Ait Nasser <140181298+aaitnasser@users.noreply.github.com>
2024-06-04 19:30:43 -0300

    Update weather_train_without.sh

--- weather_train_without.sh
+++ weather_train_without.sh
@@ -1,11 +1,10 @@
-#!/bin/bash  # The shebang line specifies the script interpreter.
-#SBATCH --cpus-per-task=1   # Allocate 1 CPU core per task.
-#SBATCH --gres=gpu:1  # Request 1 GPU for the job.
-#SBATCH --mem=16G  # Memory per node
-#SBATCH --time=0-1:00:00  # Time (DD-HH:MM)
-#SBATCH --output=%N-%j.out
-#SBATCH --mail-user=your_email_address
-#SBATCH --mail-type=ALL
+#!/bin/bash  
+#SBATCH --cpus-per-task=1   
+#SBATCH --gres=gpu:1  
+#SBATCH --mem=16G  
+#SBATCH --time=0-1:00:00  
+#SBATCH --output=test-%j.out
+
 
 # launch the train file
 python temperature_train.py
